87204d20348dab736977668023dc6b4e602befb4
SenorAKS <aktcool4@gmail.com>
2024-05-08 13:29:56 +0530

    second commit

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,12 +1,39 @@
 const mongoose=require("mongoose")
 
-mongoose.connect("mongodb://localhost:27017/LoginDatbase")
-.then(()=>{
-    console.log('mongoose connected');
+const mongoURL= 'mongodb+srv://aktcool4:Qwerty12345@cluster0.ivdvqwg.mongodb.net/'
+// const mongoURL = process.env.MONGODB_URL;
+
+// Set up MongoDB connection
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-.catch((e)=>{
-    console.log('failed');
-})
+
+// Get the default connection
+// Mongoose maintains a default connection object representing the MongoDB connection.
+const db = mongoose.connection;
+
+// Define event listeners for database connection
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB server');
+});
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+// mongoose.connect("mongodb://localhost:27017/LoginDatbase")
+// .then(()=>{
+//     console.log('mongoose connected');
+// })
+// .catch((e)=>{
+//     console.log('failed');
+// })
 
 const logInSchema=new mongoose.Schema({
     name:{
@@ -26,4 +53,5 @@ const logInSchema=new mongoose.Schema({
 
 const LogInCollection=new mongoose.model('LogInCollection',logInSchema)
 
-module.exports=LogInCollection
+module.exports=db;
+module.exports=LogInCollection;
